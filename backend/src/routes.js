3e1a8ac225380c1e8b54e1d59db70e864bbdbcc7
Helder Avila <helderavila2016@gmail.com>
2020-01-31 15:29:27 -0300

    Listando agendamentos do usuario

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -25,6 +25,7 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderControler.index);
 
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
