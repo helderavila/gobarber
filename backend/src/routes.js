ecccb6c8e8954ad0a9fe5f2f28639575c60e98b7
Helder Avila <helderavila2016@gmail.com>
2020-01-31 15:50:27 -0300

    Listando agenda do prestador

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,6 +9,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderControler from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 // Auth Middleware
 import authMiddleware from './app/middlewares/auth';
@@ -27,6 +28,8 @@ routes.get('/providers', ProviderControler.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
