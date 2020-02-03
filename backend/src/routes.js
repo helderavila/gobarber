2a4a7a269b3a46412549ff39d2e8a80eb4cfd91e
Helder Avila <helderavila2016@gmail.com>
2020-02-03 10:01:13 -0300

    Cancelamento do agendamento

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -29,6 +29,7 @@ routes.get('/providers', ProviderControler.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedule', ScheduleController.index);
 
