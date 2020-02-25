317c09dbd4bd91761dab2e2db57d3f073afdbc1f
Helder Avila <helderavila2016@gmail.com>
2020-02-25 15:13:32 -0300

    chore: use root import

--- web/src/routes/Route.js
+++ web/src/routes/Route.js
@@ -1,10 +1,9 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
-import AuthLayout from '../pages/_layouts/auth';
-import DefaultLayout from '../pages/_layouts/default';
+import AuthLayout from '~/pages/_layouts/auth';
+import DefaultLayout from '~/pages/_layouts/default';
 
 export default function RouteWrapper({
   component: Component,
