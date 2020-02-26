6519011243f50cf009c3127362ddb9418ea79687
Helder Avila <helderavila2016@gmail.com>
2020-02-26 15:12:03 -0300

    feat: user can create a account

--- web/src/store/modules/auth/sagas.js
+++ web/src/store/modules/auth/sagas.js
@@ -31,4 +31,28 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    toast.success('Cadastro criado com sucesso!');
+
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha no cadastro, verifique seus dados!');
+
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
