299a6c5e58aea5cb78652eba977edf839aea625b
Helder Avila <helderavila2016@gmail.com>
2020-02-27 13:38:37 -0300

    chore: styling notifications

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Notifications from '~/components/Notifications';
 import logo from '~/assets/logo-purple.svg';
 
 import { Container, Content, Profile } from './styles';
@@ -14,6 +15,7 @@ export default function Header() {
           <Link to="/dashboard">DASHBOARD</Link>
         </nav>
         <aside>
+          <Notifications />
           <Profile>
             <div>
               <strong>Helder Avila</strong>
