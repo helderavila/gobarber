efe5721d29d72630ac92ac8c178560843c65ce1e
Helder Avila <helderavila2016@gmail.com>
2020-03-18 13:25:37 -0300

    Att

--- web/src/store/modules/auth/reducer.js
+++ web/src/store/modules/auth/reducer.js
@@ -3,7 +3,7 @@ import produce from 'immer';
 const INITIAL_STATE = {
   token: null,
   signed: false,
-  false: false,
+  loading: false,
 };
 
 export default function auth(state = INITIAL_STATE, action) {
@@ -21,6 +21,11 @@ export default function auth(state = INITIAL_STATE, action) {
       }
       case '@auth/SIGN_FAILURE': {
         draft.loading = false;
+        break;
+      }
+      case '@auth/SIGN_OUT': {
+        draft.token = null;
+        draft.signed = false;
         break;
       }
       default:
