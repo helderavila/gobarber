95301e852142ae52bc78f882d447e6f2a1782c51
Helder <helderavila2016@gmail.com>
2020-04-01 18:21:17 -0300

    Listando agendamentos

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -26,7 +26,6 @@ export default function Header() {
             </div>
             <img
               src={
-                profile.avatar.url ||
                 'https://api.adorable.io/avatars/50/abott@adorable.png'
               }
               alt="Helder Avila"
